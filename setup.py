b0db37e0d11bfe2c7a8b066886b05ac487b2d628
Theo Dickson <tmsdickson@gmail.com>
2021-07-31 07:17:39 +0100

    Use MANIFEST.in after all

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='socomote',
+    description="Application for remote control of Sonos systems",
+    license='MIT',
+    author="Theo Dickson",
+    author_email="tmsdickson@gmail.com",
     version='0.0.4',
     packages=['socomote'],
+    include_package_data=True,
     install_requires=[
         "pyyaml",
         "soco >= 0.23.2",
         "getkey >= 0.6.5"
     ],
-    description="Application for remote control of Sonos systems",
-    author="Theo Dickson",
-    author_email="tmsdickson@gmail.com",
-    license='MIT',
-    package_data={'socomote': ['resources/example_config.yaml']}
 )
