953503b1bdaa41cdb085e14cbd894beac1aa635f
Theo Dickson <tmsdickson@gmail.com>
2021-08-01 11:33:35 +0100

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     license='MIT',
     author="Theo Dickson",
     author_email="tmsdickson@gmail.com",
-    version='0.1.0',
+    version='0.2.0',
     packages=['socomote'],
     include_package_data=True,
     install_requires=[
